b3aeb0e57b6a160bae6941935376519dde4b8e97
Ashley Haas <ashley@avironsoftware.com>
2022-05-12 14:13:27 -0500

    Ashleys Attempts

--- main.js
+++ main.js
@@ -5,6 +5,28 @@ async function getRandomQuestion(){
     const newElement = document.createElement('div')
     newElement.innerHTML = questionObject.question
     document.body.appendChild(newElement)
+    return questionObject
 }
 
-getRandomQuestion()
+async function getTheAnswer(){
+    const questionObject = getRandomQuestion()
+    const newElement = document.createElement('div')
+    newElement.innerHTML = questionObject.answer
+    document.body.appendChild(newElement)
+}
+
+function newQuestionButton() {
+    const qButton = document.createElement('button')
+    document.body.appendChild(qButton)
+    qButton.innerHTML = "New Question"
+    qButton.addEventListener("click", getRandomQuestion)
+}
+newQuestionButton()
+
+function revealAnswerButton() {
+    const aButton = document.createElement('button')
+    document.body.appendChild(aButton)
+    aButton.innerHTML = "Reveal Answer"
+    aButton.addEventListener("click", getTheAnswer )
+}
+revealAnswerButton()
